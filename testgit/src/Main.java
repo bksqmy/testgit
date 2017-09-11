1a638b243c59b73f8dcbc984f0c784f8db42f3eb
bksqmy <bksqmy@163.com>
2017-09-11 10:31:13 +0800

    1111

--- testgit/src/Main.java
+++ testgit/src/Main.java
@@ -10,9 +10,9 @@ public class Main {
         Map<String,Object> map = new HashMap<String,Object>();
         map.put("key1",1);
         map.put("key2",2);
-        map.put("key3",4);
-        map.put("key3",4);
-        map.put("key3",4);
+        map.put("key3",3);
+        map.put("key4",4);
+        map.put("key5",5);
 
         System.out.println(map);
 
